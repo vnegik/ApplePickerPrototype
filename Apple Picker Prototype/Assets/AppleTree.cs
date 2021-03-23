cd0016e365b4d85ad6019af91cb65187d845c42e
Vitali Nikalaenka <vnikalaenka@openmygame.com>
2021-03-23 22:43:53 +0300

    Остановка на сбрасывании яблок.

---
+++ Apple Picker Prototype/Assets/AppleTree.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+  [Header("Set in Inspector")]
+  // Шаблон для создания яблок
+  public GameObject applePrefab;
+  // Скорость движения яблони
+  public float speed = 1f;
+  // Расстояние, на котором должно изменяться направление движения яблони
+  public float leftAndRightEdge = 10f;
+    // Вероятность случайного изменения направления движения
+  public float chanceToChangeDirections = 0.1f;
+ // Частота создания экземпляров яблок
+  public float secondsBetweenAppleDrops = 1f;
+
+    void Start()
+    {
+        // Сбрасывать яблоки раз в секунду
+    }
+
+    void Update()
+    {
+        // Простое перемещение
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        // Изменение направления
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);// Начать движение вправо
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed);
+            // Начать движение влево
+        }
+       
+    }
+
+    void FixedUpdate()
+    {
+        // Теперь случайная смена направления привязана ко времени,
+        // потому что выполняется в FixedUpdate()
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1; // Изменение направления
+        }
+    }
+}
